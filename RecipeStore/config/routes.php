109217dafa5f6107704bc991f53bd7906ec8d8e3
LuckyCharms1444 <jonr15563@gmail.com>
2023-05-24 22:24:59 -0400

    Update routes.php
    Updated routes with correct routing

--- RecipeStore/config/routes.php
+++ RecipeStore/config/routes.php
@@ -1,4 +1,6 @@
 <?php
+//Jon Ross Richardson
+//Routing file to route to the correct places
 
 use Slim\App;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -14,8 +16,36 @@ return function (App $app) {
             $group->group('/recipe', function (RouteCollectorProxy $group) {
                 $group->get('', 'Recipe:index');
                 $group->get('/{id}', 'Recipe:view');
-                $group->get('/{id}/classes', 'Student:viewStudentClasses');
             });
+            $group->group('/ingredient', function (RouteCollectorProxy $group) {
+                $group->get('', 'Ingredient:index');
+                $group->get('/{id}', 'Ingredient:view');
+            });
+            $group->group('/category', function (RouteCollectorProxy $group) {
+                $group->get('', 'Category:index');
+                $group->get('/{id}', 'Category:view');
+            });
+            $group->group('/dietaryInformation', function (RouteCollectorProxy $group) {
+                $group->get('', 'DietaryInformation:index');
+                $group->get('/{id}', 'DietaryInformation:view');
+            });
+            $group->group('/recipeDietary', function (RouteCollectorProxy $group) {
+                $group->get('', 'RecipeDietary:index');
+                $group->get('/{id}', 'RecipeDietary:view');
+            });
+            $group->group('/recipeCategory', function (RouteCollectorProxy $group) {
+                $group->get('', 'RecipeCategory:index');
+                $group->get('/{id}', 'RecipeCategory:view');
+            });
+            $group->group('/recipeIngredient', function (RouteCollectorProxy $group) {
+                $group->get('', 'RecipeIngredient:index');
+                $group->get('/{id}', 'RecipeIngredient:view');
+            });
+            $group->group('/recipeCuisine', function (RouteCollectorProxy $group) {
+                $group->get('', 'RecipeCuisine:index');
+                $group->get('/{id}', 'RecipeCuisine:view');
+            });
+
         });
 
 // Handle invalid routes
