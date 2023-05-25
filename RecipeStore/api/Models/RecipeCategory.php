7ea74d269396d890fcc9890f20191fcad8ef67c2
LuckyCharms1444 <jonr15563@gmail.com>
2023-05-24 21:54:48 -0400

    RecipeCategory endpoints
    Endpoint created for the RecipeCategory. With inverse of the one to many

--- RecipeStore/api/Models/RecipeCategory.php
+++ RecipeStore/api/Models/RecipeCategory.php
@@ -19,5 +19,29 @@ Class RecipeCategory extends Model{
     public $timestamps = false;
 
 
+
+// Define the one to many (inverse) relationship between Recipe and RecipeCategory
+    public function recipe() {
+        return $this->belongsTo(Recipe::class, 'recipe_id');
+    }
+// Define the one to many (inverse) relationship between Category and RecipeCategory
+    public function category() {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    //Retrieve all recipe categories
+    public static function getRecipeCategories() {
+        $RP = self::with(['recipe_id', 'category_id'])->get();
+        return $RP;
+    }
+
+    //Retrieve a specific recipe by a category number
+    public static function getRecipeCategoryByID(int $category) {
+        $rc = self::findOrFail($category);
+        $rc->load('recipe_id')->load('category_id');
+        return $rc;
+    }
+
+
 };
 
